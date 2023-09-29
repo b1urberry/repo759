c5d72fd77b2ada38f9a4365c969c81e07731bd50
blurberry <48646019+dengden@users.noreply.github.com>
2023-09-28 20:51:55 -0500

    Untrack files now ignored by .gitignore

--- HW03/vscale.cu
+++
@@ -1,9 +0,0 @@
-#include "vscale.cuh"
-
-__global__ void vscale(const float *a, float *b, unsigned int n) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (tid < n) {
-        b[tid] = a[tid] * b[tid];
-    }
-}

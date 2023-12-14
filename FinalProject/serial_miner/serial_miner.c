c175691ac646a3a0f7364efca82bd93da29ca25d
blurberry <48646019+dengden@users.noreply.github.com>
2023-12-14 12:01:28 -0600

    added hashrate calculation

--- FinalProject/serial_miner/serial_miner.c
+++ FinalProject/serial_miner/serial_miner.c
@@ -13,13 +13,7 @@ int main()
     char *merkle_root = "3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A";
     char *time = "29AB5F49";
     char *bits = "FFFF001D";
-
-    unsigned char result[32];
-
-    // testing hashBlock funciton with nonce = 2083236893, the correct nonce
-    //the following line verifies the genesis block in one hash
-    print_bytes_reversed((unsigned char *)result, 32, 1);
-
+    
     // testing mineBlock function with nonce = 2083236793, 10 nounces before the correct nonce
     // the following line verifies the genesis block in 10 hashes
     uint32_t nonce = mineBlock(2000000000, version, prev_block_hash, merkle_root, time, bits);
